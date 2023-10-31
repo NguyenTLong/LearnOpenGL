d15ab37f0fab68878a75a0072af65cd5d7b17b2e
NguyenTLong <103912023+NguyenTLong@users.noreply.github.com>
2023-10-31 17:43:57 +0700

    light

--- LearnOpenGL/Main.cpp
+++ LearnOpenGL/Main.cpp
@@ -43,9 +43,6 @@ Shader lightShader;
 
 vec3 lightPos = vec3(1.2f, 1.0f, 2.0f);
 
-vec3 cameraPos = vec3(0.0f, 0.0f, 3.0f);
-vec3 cameraFront = vec3(.0f, .0f, -1.0f);
-vec3 cameraUp = vec3(.0f, .0f, .0f);
 Camera camera(vec3(0.0f, .0f, 3.0f));
 
 bool firstMouse = true;
@@ -250,16 +247,26 @@ void Render(GLFWwindow* window) {
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		ourShader.use();
+		ourShader.setVec3("light.position", camera.Position);
+		ourShader.setVec3("light.direction", camera.Front);
+		ourShader.setFloat("light.cutOff", cos(radians(12.5f)));
+		ourShader.setFloat("light.outerCutOff", cos(radians(17.5f)));
+
 		ourShader.setVec3("objectColor", vec3(1.0f, 1.0f, 1.0f));
 		ourShader.setVec3("viewPos", camera.Position);
 
 		ourShader.setVec3("material.ambient", vec3(1.0f, 0.5f, 0.31f));
-		ourShader.setFloat("material.shininess", 32.0f);
 
-		ourShader.setVec3("light.ambient", vec3(.2f));
-		ourShader.setVec3("light.diffuse", vec3(.5f));
+		ourShader.setVec3("light.ambient", vec3(.1f));
+		ourShader.setVec3("light.diffuse", vec3(.8f));
 		ourShader.setVec3("light.specular", vec3(1.0f));
-		ourShader.setVec3("light.position", lightPos);
+
+
+		ourShader.setFloat("light.constant", 1.0f);
+		ourShader.setFloat("light.linear", 0.09f);
+		ourShader.setFloat("light.quadratic", 0.032f);
+
+		ourShader.setFloat("material.shininess", 32.0f);
 
 
 		mat4 projection = mat4(1.0f);
@@ -279,9 +286,20 @@ void Render(GLFWwindow* window) {
 
 		glActiveTexture(GL_TEXTURE1);
 		glBindTexture(GL_TEXTURE_2D, specularMap);
-
+		
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 36);
+
+		for (unsigned int i = 0; i < 10; i++)
+		{
+			glm::mat4 model = glm::mat4(1.0f);
+			model = glm::translate(model, cubePositions[i]);
+			float angle = 20.0f * i;
+			model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+			ourShader.setMat4("model", model);
+
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+		}
+		//glDrawArrays(GL_TRIANGLES, 0, 36);
 
 
 		lightShader.use();
--- LearnOpenGL/frag_container.glsl
+++ LearnOpenGL/frag_container.glsl
@@ -16,9 +16,18 @@ uniform Material material;
 
 struct Light {
     vec3 position;
+    vec3 direction;
+
+    float cutOff;
+    float outerCutOff;
+
     vec3 ambient;
     vec3 diffuse;
     vec3 specular;
+
+    float constant;
+    float linear;
+    float quadratic;
 };
 uniform Light light;
 
@@ -28,12 +37,14 @@ in vec3 FragPos;
 
 void main()
 {
+    
     // ambient
     vec3 ambient =vec3(texture(material.diffuse, TexCoords)) * light.ambient;
     
     // diffuse
     vec3 norm = normalize(Normal);
     vec3 lightDir = normalize(light.position - FragPos);
+//    vec3 lightDir = normalize(-light.direction);
 
 
     float diff = max(dot(norm, lightDir), 0.0);
@@ -46,6 +57,21 @@ void main()
     float spec = pow(max(dot(viewDir, reflectDir), 0.0),material.shininess);
     vec3 specular = light.specular * (spec * vec3(texture(material.specular, TexCoords)));
 
-    vec3 result = (ambient + diffuse + specular) * objectColor;
+    float theta = dot(lightDir, normalize(-light.direction));
+    float epsilon = light.cutOff - light.outerCutOff;
+    float intensity = clamp((theta - light.outerCutOff) / epsilon, 0.0, 1.0);
+
+    diffuse *= intensity;
+    specular *= intensity;
+
+    float dis = length(light.position - FragPos);
+    float attenuation = 1.0/ (light.constant + light.linear * dis + light.quadratic * (dis * dis));
+
+    ambient *= attenuation;
+    diffuse *= attenuation;
+    specular *= attenuation;
+
+    vec3 result = (ambient + diffuse + specular) ;
     FragColor = vec4(result, 1.0);
+
 }
